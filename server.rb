63bfdbc7e61da405d0ec88d086f18d8ffbdad2b7
Yuri Feldman <yuri@tbqh.net>
2011-08-22 00:47:51 +1000

    testing instapaper auth

--- server.rb
+++ server.rb
@@ -5,15 +5,34 @@ require 'rss/1.0'
 require 'rss/2.0'
 require 'open-uri'
 
+require 'net/https'
+
 get '/' do
   content_type :html
   erb :index
 end
 
 post '/add' do
-  params[:url]
+  params[:url] = "http://feeds.feedburner.com/rudiusmedia/rch" # Dev link
   rss = get_rss(params[:url])
-  erb rss.channel.title
+  @items = rss.items
+  @channel = rss.channel
+  erb :list
+end
+
+get '/auth' do
+  erb :auth
+end
+
+post '/testauth' do
+  res = Net::HTTP.post_form(URI.parse("http://www.instapaper.com/api/authenticate"),
+  {'username' => params[:username], 'password' => params[:password]})
+  case res
+  when Net::HTTPSuccess, Net::HTTPRedirection
+    "Good!"
+  else
+    "Oh noes, incorrect username/pass"
+  end
 end
 
 def get_rss(url)

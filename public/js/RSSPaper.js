e691d7b0b8884bc7a8121c0546cb69820c4bb3dc
Yuri Feldman <yuri@tbqh.net>
2011-08-22 04:12:41 +1000

    basic backbone load of rss feed

--- public/js/RSSPaper.js
+++ public/js/RSSPaper.js
@@ -1,2 +1,82 @@
+
 (function($){
-})(jQuery)
+  _.templateSettings = {
+    interpolate : /\{\{(.+?)\}\}/g
+  };
+  
+  window.RSSFeed = Backbone.Model.extend({
+    initialize: function(){
+      
+      console.log(this.get('title'));
+    }
+  });
+  
+  window.RSSFeeds = Backbone.Collection.extend({
+    model: RSSFeed,
+    url: "/rss"
+  });
+  
+  window.Feeds = new RSSFeeds();
+  
+  $(document).ready(function() {
+    
+    window.RSSView = Backbone.View.extend({
+      template: _.template($('#rss-feed-template').html()),
+      tag: 'li',
+      initialize: function() {
+        _.bindAll(this, 'render');
+      },
+      
+      render: function() {
+        $(this.el).html(this.template(this.model.toJSON()));
+        return this;
+      }
+    });
+    
+    window.RSSListView = Backbone.View.extend({
+      tagName: 'section',
+      template: _.template($('#feeds-template').html()),
+      initialize: function() {
+        _.bindAll(this, 'render');
+        this.collection.bind('reset', this.render);
+      },
+      
+      render: function() {
+       var $feeds;
+       collection = this.collection;
+       
+       $(this.el).html(this.template({}));
+       $feeds = this.$(".feeds");
+       this.collection.each(function(feed) {
+         var view = new RSSView({
+           model: feed,
+           collection: collection
+         });
+         $feeds.append(view.render().el);
+       });
+       
+       return this; 
+      }
+    });
+    
+    window.RSSPaper = Backbone.Router.extend({
+      routes: {
+        '': 'home'
+      },
+      
+      initialize: function() {
+        console.log('yes');
+        this.rssListView = new RSSListView({
+          collection: window.Feeds
+        });
+      },
+      
+      home: function() {
+        $('#container').empty();
+        $('#container').append(this.rssListView.render().el);
+      }
+    });
+
+  });
+  
+})(jQuery);
--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'json'
 
 require 'rss/1.0'
 require 'rss/2.0'
@@ -12,12 +13,11 @@ get '/' do
   erb :index
 end
 
-post '/add' do
+get '/rss' do
   params[:url] = "http://feeds.feedburner.com/rudiusmedia/rch" # Dev link
   rss = get_rss(params[:url])
-  @items = rss.items
-  @channel = rss.channel
-  erb :list
+  content_type :json
+  rss.to_json
 end
 
 get '/auth' do
@@ -41,4 +41,18 @@ def get_rss(url)
     response = http.read
   end
   RSS::Parser.parse(response, false)
+end
+
+class RSS::Rss
+  def to_json
+    ret = Array.new()
+    items.each do |rss|
+      ret << {
+        "title"       => rss.title,
+        "link"        => rss.link,
+        "description" => rss.description
+      }
+    end
+    ret.to_json
+  end
 end
